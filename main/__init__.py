f3aee68fbb6fb82bc14c14c025eec25fa347d604
xianlinlc <avicii950818@gmail.com>
2023-03-04 19:44:44 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 20107940("API_ID", default=None, cast=int)
-API_HASH = "a94f07a23f7ff3cbddb70ec1276bcb57"("API_HASH", default=None)
-BOT_TOKEN = "6146314523:AAGQhbsWthN0fI-3-Ro5acRqSIJaxp8aI3w"("BOT_TOKEN", default=None)
-SESSION = "BQBkb60wbFXuLbk1zY7ugsw3ue5RHcXpdAzHFAMeFyZGSmhERpP6F18mF9Or_PN7IWwih9I_YD1axW_AhJTlTtRdDiT-j_De4rP0yNKtOa1P-hNRm_dYfZq0s9foK7yiKnI_kGf4wT4QBrWOJRIJXNw7P2TcgnGNp2g5ljtzYeSZ4xR355oaQI-fSXUMiqDJMNq6dWT2ZPtcTyBxxPA72O55EQQtsVQz9qZDchTF3CPnjQClxgS8xNzQwEKjBrX6P2ze26lhCbehs0y3dB4YTJvG5TFEufvPAb37SBpOzAVYL692hjxOeeJqpNu-iKsoxrpHhOkjYpw4zM2cMiJYOomQNvVupgA"("SESSION", default=None)
-FORCESUB = "lc9619520"("FORCESUB", default=None)
-AUTH = 922054310("AUTH", default=None, cast=int)
+API_ID = 20107940
+API_HASH = "a94f07a23f7ff3cbddb70ec1276bcb57"
+BOT_TOKEN = "6146314523:AAGQhbsWthN0fI-3-Ro5acRqSIJaxp8aI3w"
+SESSION = "BQBkb60wbFXuLbk1zY7ugsw3ue5RHcXpdAzHFAMeFyZGSmhERpP6F18mF9Or_PN7IWwih9I_YD1axW_AhJTlTtRdDiT-j_De4rP0yNKtOa1P-hNRm_dYfZq0s9foK7yiKnI_kGf4wT4QBrWOJRIJXNw7P2TcgnGNp2g5ljtzYeSZ4xR355oaQI-fSXUMiqDJMNq6dWT2ZPtcTyBxxPA72O55EQQtsVQz9qZDchTF3CPnjQClxgS8xNzQwEKjBrX6P2ze26lhCbehs0y3dB4YTJvG5TFEufvPAb37SBpOzAVYL692hjxOeeJqpNu-iKsoxrpHhOkjYpw4zM2cMiJYOomQNvVupgA"
+FORCESUB = "lc9619520"
+AUTH = 922054310
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
